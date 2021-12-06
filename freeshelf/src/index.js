647a49e86bf2ba7175ac05e09ce6f8f7a112471c
Jordyn <jordyn@example.com>
2021-12-06 09:46:46 -0500

    trying to figure out JS

--- freeshelf/src/index.js
+++ freeshelf/src/index.js
@@ -1,5 +1,5 @@
 // creating custom components for each section 
-<body>
+{/* <body>
     <div id="root"></div>
     <script src="https://unpkg.com/react@16.12.0/umd/react.development.js"></script>
     <script src="https://unpkg.com/react-dom@16.12.0/umd/react-dom.development.js"></script>
@@ -18,7 +18,7 @@
 
 </script>
     
-</body>
+</body> */}
 
 
 
@@ -35,36 +35,36 @@
 
 
 
-{/* following example from class  */}
-// // import React from 'react'
-// import ReactDOM from 'react-dom'
-// import { useState } from 'react'
+// {/* following example from class  */}
+// // // import React from 'react'
+// // import ReactDOM from 'react-dom'
+// // import { useState } from 'react'
 
 
-// const App = () => {
-//     const [authors] = useState(['Kyle Simpson','Addy Osmani','Caleb Doxsy','Chris Pine'])
-//     return (
-//         <>
-//             <h1> You Don't Know JS: Up and Going</h1>
-//             {authors.map((author,index) => (
-//                 <Author authorName={author} key={index} />
-//             ))}
-//             <div>If you're just getting into programming and/or JavaScript, this book will breifly explore what you need to get up and going</div>
-//             <button>More information</button>
-//         </>
-//     )
-// }
+const App = () => {
+    const [authors] = useState(['Kyle Simpson','Addy Osmani','Caleb Doxsy','Chris Pine'])
+    return (
+        <>
+            <h1> You Don't Know JS: Up and Going</h1>
+            {authors.map((author,index) => (
+                <Author authorName={author} key={index} />
+            ))}
+            <div>If you're just getting into programming and/or JavaScript, this book will breifly explore what you need to get up and going</div>
+            <button>More information</button>
+        </>
+    )
+}
 
-// const Author = (props) => {
-//     console.log(props)
-//     return (
-//         <div className='author'>
-//             <p>{props.authorName}</p>
-//             <p>More Information</p>
-//         </div>
+const Author = (props) => {
+    console.log(props)
+    return (
+        <div className='author'>
+            <p>{props.authorName}</p>
+            <p>More Information</p>
+        </div>
 
-//     )
+    )
 
-// }
+}
 
-// ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'))
